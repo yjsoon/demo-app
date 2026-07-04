fd16897c8dbf4b0c150c93e73b0cd80defe1977a
YJ Soon <yjsoon@tinkertanker.com>
2026-07-04 10:15:45 +0800

    Added background image and name

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.red
+            Text("YJ Soon")
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .bold()
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
